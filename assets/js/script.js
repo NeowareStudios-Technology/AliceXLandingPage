e876c2a1d72fdaf7febb56496f046d2e9f0a80cb
Ron <rforcier2@gmail.com>
2018-10-03 17:26:44 -0400

    increase play speed on Video
    Increased playback speed on How to Create Section video

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,6 @@
 /*
 	LWA alice one landing page script
 */
-$(window).on('scroll', function() {
-	// $parrot = $('#parrotModel');
-	// $pCont = $('.parrotContainer');
-	// var offset = $pCont.offset();
-	// $pCont.html( "left: " + offset.left + ", top: " + offset.top );
-	// $parrot.show();
-	// 	if( $(this).scrollTop() == $parrot.position().top ){
-	//       $parrot.animate({
-	//     		opacity: 0.25,
-	//     		left: "+=50",
-	// 		    height: "toggle"
-	// 		  }, 5000, function() {
-	// 		    // Animation complete
-	//
-	// 		  });
-	// 	}
-})
+
+var vid1 = document.getElementById("video1");
+vid1.playbackRate = 2.0;
